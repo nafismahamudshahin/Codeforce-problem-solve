3fcd9fb85a0ab40235c994232e5058c5a1b5a399
nafismahamudshahin <nafismahamudshahin@gmail.com>
2024-11-13 00:37:57 +0600

    solve 800 rating problem name of Good Kid

---
+++ B_Good_Kid.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fast ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define all(x) x.begin(), x.end()
+#define ll long long int
+#define endl '\n'
+int main(){
+    fast;
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int>v(n);
+        for(int i=0;i<n;i++){
+            cin>>v[i];
+        }
+        sort(all(v));
+        v[0]+=1;
+        int ans =1;
+        for(auto i:v){
+            ans*=i;
+        }
+        cout<<ans<<endl;
+    }
+    return 0;
+}
